40ed62fe31bb573a92d974e6a21556c9dc3daffb
ivan <ivan-guita@hotmail.com>
2023-12-23 21:39:45 -0300

    Rotas get funcionando

--- index.js
+++ index.js
@@ -2,7 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.listen(4000,function(){
+/*
+  Atraves do express e possivel criar rotas de manira rapida
+  a rota abaixo e do tipo get a função get pode receber 2 
+  parametros 1 eo link de requisição que vem do usuario
+  para acessar ela 
+  
+  eo segundo parametro é um função anonima que recebe 2 parametros 
+  req(requisição)
+  res(resposta)
+  dependendo do link de rsuisição acionaod a função vai decidir oque fazer
+  no caso se o cliente requisitar / ela vai mandar um texto de boas vindas a pagina de inicio   
+*/
+
+app.get('/',function(req,res){
+    res.send('Bem vindo a pagina inicial');
+})
+
+
+app.get('/blog',function(req,res){
+    res.send('Bem vindo a o meu blog');
+})
+
+app.listen(4000,function(erro){
     if(erro){
         console.log('Ocorreu um erro');
     }else{
--- teoria/Teoria.js
+++ teoria/Teoria.js
@@ -1,6 +1,25 @@
 /*
  O express é um framework que premite a criação de servidores 
  mas essa não e unica utilidade dele em relação a criação de aplicações
- no Node  
+ no Node.
+ 
+ Rotas
+
+ /*
+  Atraves do express e possivel criar rotas de manira rapida
+  a rota abaixo e do tipo get a função get pode receber 2 
+  parametros 1 eo link de requisição que vem do usuario
+  para acessar ela 
+  
+  eo segundo parametro é um função anonima que recebe 2 parametros 
+  req(requisição)
+  res(resposta)
+  dependendo do link de rsuisição acionaod a função vai decidir oque fazer
+  no caso se o cliente requisitar / ela vai mandar um texto de boas vindas a pagina de inicio   
+*/
+
+app.get('/',function(req,res){
+    res.send('Bem vindo a pagina inicial');
+})
 
 */
