042b9babacc838c3975a9c01e6665fd17bfc4757
ivan <ivan-guita@hotmail.com>
2023-12-25 10:27:19 -0300

    inicio da aula sobre rotas com o express

--- teoria/Teoria.js
+++ teoria/Teoria.js
@@ -16,10 +16,25 @@
   res(resposta)
   dependendo do link de rsuisição acionaod a função vai decidir oque fazer
   no caso se o cliente requisitar / ela vai mandar um texto de boas vindas a pagina de inicio   
-*/
+
 
 app.get('/',function(req,res){
     res.send('Bem vindo a pagina inicial');
 })
+
+*/
+
+/*
+ Rotas que recebenm parametros
+
+  Os parametros são necessarios em rotas que mosttram os dados de um porduto,usuario ou artigo especifico,
+  os parametros servem pra rota saber qoue deve mostrar ao usuario, digamos que estamois em um site de compras
+  eu vejo uma pagina com varios produtos, mas eu quero saber mais, sobre um produto especifico.
+  
+  quando clico sobre ele o site tem que nme levar a uma pagina de detalhes desse produto e com as opções de 
+  compra possiveis.mas para que isso aconteça a rota tem que receber os dados desse produto pra poder mostrar.
+
+ 
+
 
 */
